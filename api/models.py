ac5ce458ed18aa0561df2acb34af13765a63c0d4
tsepanx <gitg@riseup.net>
2020-05-21 12:12:45 +0300

    Models for user & post

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    status = models.CharField(max_length=100)
+    profile_photo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'@{self.username}, status: {self.status}'
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    title = models.CharField(max_length=30, verbose_name='Post title')
+    text = models.TextField(verbose_name='Post text')
+
+    pub_date = models.DateTimeField(verbose_name='Post published', auto_now=True)
+
+    def __str__(self):
+        return f'@{self.user.username}, {self.title}, {self.pub_date}'
