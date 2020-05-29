dafd1a3ec110a87aecbc61a47b6253a6bca7b61d
tsepanx <77626319+tsepanx@users.noreply.github.com>
2020-05-29 11:13:41 +0300

    Nobody custom permission for profile POST method

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,5 @@
+from rest_framework import permissions
+
 from .serializers import UserGETSerializer
 
 
@@ -6,3 +8,8 @@ def my_jwt_response_handler(token, user=None, request=None):
         'token': token,
         'user': UserGETSerializer(user, context={'request': request}).data
     }
+
+
+class Nobody(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return False
