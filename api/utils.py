f3ccb40fd095fba1588fc10baffcc751491b8ef9
tsepanx <77626319+tsepanx@users.noreply.github.com>
2020-06-02 23:41:44 +0300

    rm ustils.py with empty file

--- api/utils.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import permissions, authentication
-
-
-class ExampleAuthentication(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        pass
-        # raise exceptions.AuthenticationFailed()
