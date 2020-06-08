f8fee3b114bdceaf27729c6750e9d747b68b5889
tsepanx <gitg@riseup.net>
2020-06-08 10:27:08 +0300

    friends field for retrieve of profile

--- api/serializers/social_user.py
+++ api/serializers/social_user.py
@@ -4,21 +4,25 @@ from api.models import SocialUser
 from . import profile
 
 
+def get_friends_of(user: SocialUser):
+    for social_user in user.relationships.all():
+        yield profile.WithUsernameSerializer(social_user.person.profile).data
+
+
 class SocialUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = SocialUser
-        exclude = ()
+        exclude = ('person',)
 
 
 class RetrieveSerializer(SocialUserSerializer):
     friends = serializers.SerializerMethodField()
 
     def get_friends(self, obj):
-        for social_user in obj.relationships.all():
-            yield profile.WithUsernameSerializer(social_user.person.profile).data
+        return get_friends_of(obj)
 
     class Meta(SocialUserSerializer.Meta):
-        exclude = ('relationships',)
+        exclude = ('relationships', 'person')
 
 
 METHODS_SERIALIZERS = {
